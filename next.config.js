635b302e20bfb69fb9054646b32430d727de8ecb
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-09-15 08:15:46 +0530

    typescript ignore while building

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
